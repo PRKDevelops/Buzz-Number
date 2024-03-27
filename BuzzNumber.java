e956cfc6834a73391f5fee3b9f8bee058f2c8194
PRKDevelops <124041839+PRKDevelops@users.noreply.github.com>
2024-03-27 19:48:50 +0530

    BuzzNumber.java

--- BuzzNumber.java
+++ BuzzNumber.java
@@ -1,4 +1,27 @@
 
-public class BuzzNumber {
+import java.util.*;
+import java.io.*;
+import java.util.Scanner;
 
+public class BuzzNumber {
+  static boolean checkNumber(int number){
+        if(number % 10 == 7 || number % 7 == 0){
+            return true;
+        } else 
+        return false;
+        
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n1,n2;
+        n1 = sc.nextInt();
+    
+        if(checkNumber(n1)){
+            System.out.println(n1+"Is a Buzz number");
+        }
+        else{
+            System.out.println(n1+"is not a buzz number");
+        }
+    }
 }
+
